fbc686d5b8b75ef74e87d4085ffca2b84ad2e1da
Aditya Prasad Soni <prasad.adityas45@gmail.com>
2026-08-12 22:48:51 +0530

    Add 02_where.sql

--- 02_where.sql
+++ 02_where.sql
@@ -1,39 +1,39 @@
 -- Find all customers whose score is greater than 500
 SELECT *
-FROM [dbo].[customers]
+FROM customers
 WHERE score > 500;
 
 -- Find all customers whose score is greater than or equal to 500.
 SELECT *
-FROM [dbo].[customers]
+FROM customers
 WHERE score >= 500;
 
 -- Find all customers who are from Germany.
 SELECT *
-FROM [dbo].[customers]
+FROM customers
 WHERE country = 'Germany';
 
 -- Find customers who are from Germany AND have a score greater than 400.
 SELECT *
-FROM [dbo].[customers]
+FROM customers
 WHERE country = 'Germany' AND score > 400;
 
 -- Find customers who are from Germany OR have a score greater than 800.
 SELECT *
-FROM [dbo].[customers]
+FROM customers
 WHERE country = 'Germany' OR score > 800;
 
 -- Find customers who are NOT from the USA and have a score greater than 300.
 SELECT *
-FROM [dbo].[customers]
-WHERE country <> 'USA' AND score > 300;
+FROM customers
+WHERE country = 'USA' AND score > 300;
 
 -- Find all orders where sales is greater than 15
 SELECT *
-FROM [dbo].[orders]
+FROM orders
 WHERE sales > 15;
 
 -- Find all orders placed after 2021-04-01
 SELECT *
-FROM [dbo].[orders]
+FROM orders
 WHERE order_date > '2021-04-01';
